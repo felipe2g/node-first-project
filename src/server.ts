cbed522ebd92633d4ddc1a91e274c79f357e1abd
Felipe Guimarães <felipeguimaraes2406@gmail.com>
2020-07-27 20:56:58 -0300

    added ignore watch to dev:server

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 
 app.get('/', (request, response) => {
-    return response.json({ message: 'hello world'});
+    return response.json({ message: 'hello Felipe!'});
 })
 
 app.listen(3333, () => {
