7cb35cbecaf1f3a80caa9941722230f3387a9bbb
Felipe Guimarães <felipeguimaraes2406@gmail.com>
2020-07-27 21:22:29 -0300

    added prettier config

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,9 @@ import routes from './routes';
 
 const app = express();
 
-app.get('/', (request, response) => response.json({ message: 'hello Felipe!' }));
+app.get('/', (request, response) =>
+  response.json({ message: 'hello Felipe!' }),
+);
 
 app.listen(3333, () => {
   console.log('Server started on port 3333!');
