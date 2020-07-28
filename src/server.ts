9a8e6289fc207ba26ad4a2ca6971d6c01174f11f
Felipe Guimarães <felipeguimaraes2406@gmail.com>
2020-07-27 21:12:46 -0300

    added eslint config

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,12 @@
 import express from 'express';
 
+// src/server.ts
+import routes from './routes';
+
 const app = express();
 
-app.get('/', (request, response) => {
-    return response.json({ message: 'hello Felipe!'});
-})
+app.get('/', (request, response) => response.json({ message: 'hello Felipe!' }));
 
 app.listen(3333, () => {
-    console.log('Server started on port 3333!')
-})
+  console.log('Server started on port 3333!');
+});
