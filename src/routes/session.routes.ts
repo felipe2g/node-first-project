2447dc9dc37ffce489b008c108fe53a2179b2d1a
Felipe Guimarães <felipeguimaraes2406@gmail.com>
2020-08-01 15:56:51 -0300

    added authentication service, working fine

---
+++ src/routes/session.routes.ts
@@ -0,0 +1,21 @@
+import { Router } from 'express';
+
+import AuthenticateUserService from '../service/AuthenticateUserService';
+
+const sessionsRouter = Router();
+
+sessionsRouter.post('/', async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const authenticateUser = new AuthenticateUserService();
+
+    const { user } = await authenticateUser.execute({ email, password });
+
+    return res.json({ user });
+  } catch (err) {
+    return res.status(400).json({ error: err.message });
+  }
+});
+
+export default sessionsRouter;
