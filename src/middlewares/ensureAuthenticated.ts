24346966dddd413fe491c62dc0bb88a071d09a9f
Felipe Guimarães <felipeguimaraes2406@gmail.com>
2020-08-01 16:39:20 -0300

    send to all routes with use ensureAuthenticated middleware user id

--- src/middlewares/ensureAuthenticated.ts
+++ src/middlewares/ensureAuthenticated.ts
@@ -3,6 +3,12 @@ import { verify } from 'jsonwebtoken';
 
 import authConfig from '../config/auth';
 
+interface TokenPayload {
+  iat: number;
+  exp: number;
+  sub: string;
+}
+
 export default function ensureAuthenticated(
   req: Request,
   res: Response,
@@ -20,6 +26,12 @@ export default function ensureAuthenticated(
 
   try {
     const decoded = verify(token, authConfig.jwt.secret);
+
+    const { sub } = decoded as TokenPayload;
+
+    req.user = {
+      id: sub,
+    };
 
     return next();
   } catch {
