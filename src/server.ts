0dd6dcefcbcda4e85555d9edbb5a99ff94518f46
Felipe Guimarães <felipeguimaraes2406@gmail.com>
2020-08-01 22:51:57 -0300

    added route with static files to show avatars

--- src/server.ts
+++ src/server.ts
@@ -2,16 +2,14 @@ import 'reflect-metadata';
 
 import express from 'express';
 import routes from './routes/index';
+import uploadConfig from './config/upload';
 
 import './database/index';
 
 const app = express();
 app.use(express.json());
+app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
-
-app.get('/', (request, response) =>
-  response.json({ message: 'hello Felipe!' }),
-);
 
 app.listen(3333, () => {
   console.log('Server started on port 3333!');
